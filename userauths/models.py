7ef6bcf9d204d97bcea91229ef07c428f7552d91
Islam412 <islam.124.hamdy@gmail.com>
2024-07-13 20:44:09 +0300

    update profile models p2

--- userauths/models.py
+++ userauths/models.py
@@ -3,7 +3,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-from shortuuid.django import ShortUUIDField
+from shortuuid.django_fields import ShortUUIDField
+import shortuuid
+
 from PIL import Image
 
 
