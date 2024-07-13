88d0278ab070339c2e09afbb6a6066f6347d3bdb
Islam412 <islam.124.hamdy@gmail.com>
2024-07-14 02:51:45 +0300

    add search in admin

--- userauths/admin.py
+++ userauths/admin.py
@@ -4,6 +4,8 @@ from userauths.models import User , Profile
 
 class Profileadmin(admin.ModelAdmin):
     list_display = ('user','first_name','last_name','username','phone','address','city','country','verified')
+    list_editable = ['verified']
+    search_fields = ('first_name', 'last_name', 'username', 'phone', 'address', 'city', 'country')
     
 class Useradmin(admin.ModelAdmin):
     list_display = ('email','first_name','last_name','username')
