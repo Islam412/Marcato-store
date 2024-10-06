a75d27f175ad4732f615cfbc95686c8841f22224
Islam412 <islam.124.hamdy@gmail.com>
2024-10-07 01:38:04 +0300

    create UserRegisterForm

---
+++ userauths/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from userauths.models import User , Profile
+
+
+class UserRegisterForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': '', 'id': "", 'placeholder':'First Name'}), max_length=100, required=True)
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': '', 'id': "", 'placeholder':'Last Name'}), max_length=100, required=True)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': '', 'id': "", 'placeholder':'Username'}), max_length=100, required=True)
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': '' , 'id': "", 'placeholder':'Email Address'}), required=True)
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'id': "", 'placeholder':'Password'}), required=True)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'with-border'
