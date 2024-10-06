d34622bcf3d39b77b1adca795826b5a26ffdafbb
Islam412 <islam.124.hamdy@gmail.com>
2024-10-07 00:33:52 +0300

    add Custom apps

--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+   
+    # Custom apps
+    'userauths',
+    'products',
+    'orders',
+    'settings',
+    
+    # Third party apps
 ]
 
 MIDDLEWARE = [
