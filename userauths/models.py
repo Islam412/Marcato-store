ff3029237067bd1019b0841312ab6acc3890480f
Islam412 <islam.124.hamdy@gmail.com>
2024-10-07 01:21:19 +0300

    create user models p1

--- userauths/models.py
+++ userauths/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    username = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    email = models.EmailField(unique=True)
+
+    # Change defult django in adminbanal
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return str(self.username)
