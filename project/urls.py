b738d490d069e81cb7bf919065e9fed1d6241358
Islam412 <islam.124.hamdy@gmail.com>
2024-10-07 01:51:52 +0300

    call userauth at urls main

--- project/urls.py
+++ project/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
+    path('user/',include('userauths.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
