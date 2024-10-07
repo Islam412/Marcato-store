918bc25ede94f50d1b35dc8c99e4d72c66464c8b
Islam412 <islam.124.hamdy@gmail.com>
2024-10-07 15:58:25 +0300

    add base file

--- project/settings.py
+++ project/settings.py
@@ -109,6 +109,18 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     "default": {
+#         'ENGINE': 'django.db.backends.mysql',
+#         "NAME": "Marcato-store",
+#         "USER": "root",
+#         "PASSWORD": "1111",
+#         "HOST": "127.0.0.1",
+#         "PORT": "3306",
+#     }
+# }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
