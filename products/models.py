c687f331e45a85d1d9762a994210b3faa13cf5c7
Islam412 <islam.124.hamdy@gmail.com>
2024-10-07 16:04:02 +0300

    create Product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from taggit.managers import TaggableManager
+
+from userauths.models import User
 
 # Create your models here.
+
+
+FLAG_TYPES=[
+    ('Sale','Sale'),
+    ('New','New'),
+    ('Feature','Feature'),
+]
+
+class Product(models.Model):
+    name = models.CharField(_('Name'),max_length=120)
+    flag = models.CharField(_('Flag'),max_length=10,choices=FLAG_TYPES)
+    image = models.ImageField(_('Image'),upload_to='products')
+    price = models.FloatField(_('Price'))
+    sku = models.CharField(_('SKU'),max_length=12)
+    subtitle = models.CharField(_('Subtitle'),max_length=300)
+    descripition = models.TextField(_('Descripition'),max_length=40000)
+    quantity = models.IntegerField(_('Quantity'))
+    brand = models.ForeignKey('Brand',verbose_name=_('Brand'), related_name='product_name', on_delete=models.SET_NULL, null = True)
+    tags = TaggableManager()
+    slug = models.SlugField(null=True,blank=True)
+    
+    def __str__(self):
+        return self.name
