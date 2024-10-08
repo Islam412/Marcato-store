4c21ba1e001a1effcd292b5bd4919401748e1311
Islam412 <islam.124.hamdy@gmail.com>
2024-10-08 04:10:44 +0300

    add reviews off revies

--- products/views.py
+++ products/views.py
@@ -11,4 +11,10 @@ class ProductList(ListView):
 class ProductDetails(DetailView):
     model = Product
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["reviews"] = Review.objects.filter(product=self.get_object())
+        return context
+    
+    
     
