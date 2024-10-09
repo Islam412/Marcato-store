b64d068a8cfc1932facb9eb81e2a028faa977814
Islam412 <islam.124.hamdy@gmail.com>
2024-10-09 15:46:49 +0300

    call brand_list urls

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProductDetails, ProductList
+from .views import ProductDetails, ProductList, BrandList
 
 app_name = 'products'
 
 urlpatterns = [
     path('', ProductList.as_view(), name='product_list'),
     path('<slug:slug>/', ProductDetails.as_view(), name='product_detail'),
+    path('brand/', BrandList.as_view(), name='brand_list'),
 ]
