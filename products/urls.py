a33a8b1ebc1ccdd2f7e26154d1440aec30db56d3
Islam412 <islam.124.hamdy@gmail.com>
2024-10-10 01:01:15 +0300

    create brand detail url

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductDetails, ProductList, BrandList
+from .views import ProductDetails, ProductList, BrandList, BrandDetails
 
 app_name = 'products'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('brands/', BrandList.as_view(), name='brand_list'),
     path('', ProductList.as_view(), name='product_list'),
     path('<slug:slug>/', ProductDetails.as_view(), name='product_detail'),
+    path('<slug:slug>/', BrandDetails.as_view(), name='brand_detail'),
 ]
