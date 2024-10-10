d7de4d203d29900bfe0d42dc31120e4347b3bde4
Islam412 <islam.124.hamdy@gmail.com>
2024-10-11 02:00:15 +0300

    fix brand img erorr

--- products/models.py
+++ products/models.py
@@ -36,6 +36,13 @@ class Product(models.Model):
        self.slug = slugify(self.name)
        super(Product, self).save(*args, **kwargs)
        
+    @property
+    def average_rating(self):
+        reviews = self.review_product.all()
+        if reviews.count() > 0:
+            total_rating = sum(review.rate for review in reviews)
+            return total_rating / reviews.count()
+        return 0  
     
 
 
