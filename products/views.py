5f490745fa02aff12beaa11fa47da8284def8b45
Islam412 <islam.124.hamdy@gmail.com>
2024-10-11 04:25:19 +0300

    return data off brand at brand details bage

--- products/views.py
+++ products/views.py
@@ -32,3 +32,9 @@ class BrandDetails(ListView):
     def get_queryset(self):
         brand = Brand.objects.get(slug=self.kwargs['slug'])
         return super().get_queryset().filter(brand=brand)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["brand"] = Brand.objects.get(slug=self.kwargs['slug'])
+        return context
+    
