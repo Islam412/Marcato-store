94d28c8af5cf898532e02d94be9a4c2cbfb9d82e
Islam412 <islam.124.hamdy@gmail.com>
2024-10-14 04:22:31 +0300

    add django queryseatapi method

--- products/views.py
+++ products/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import ListView , DetailView
+from django.db.models import Q , F
+from django.db.models.aggregates import Max,Min,Count,Avg,Sum,Value
+
 
 from .models import Product, Brand, ProductImage, Review
 
-from django.db import models
 
 
 
 def queryset_debug(request):
     
-    data = Product.objects.all()
+    # data = Product.objects.all()
+    
+    # data = Product.objects.filter(price__gt=80, quantity__lt=10)  #and
+    
+    # data = Product.objects.filter(Q(price__gt=80) & Q(quantity__lt=10))  #or
+    
+    data = Product.objects.annotate(price_with_tax=F('price')*1.2) # add new tower
     
     return render(request, 'products/queryset_debug.html', {'data': data})
 
