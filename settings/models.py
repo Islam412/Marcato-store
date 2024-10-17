49abfb4e7cd67f7391a210be09034c01e8e87525
Islam412 <islam.124.hamdy@gmail.com>
2024-10-17 03:51:36 +0300

    create models company p1

--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='company_logos')
+    subtitle = models.CharField(max_length=500)
+    facebook_link = models.URLField(max_length=200, null=True, blank=True)
+    instgram_link = models.URLField(max_length=200, null=True, blank=True)
+    twitter_link = models.URLField(max_length=200, null=True, blank=True)
+    youtube_facebook_link = models.URLField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    phones = models.CharField(max_length=255, null=True, blank=True)
+    android_app = models.URLField(max_length=200, null=True, blank=True)
+    ios_app = models.URLField(max_length=200, null=True, blank=True)
+    call_us = models.CharField(max_length=255, null=True, blank=True)
+    email_us = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
